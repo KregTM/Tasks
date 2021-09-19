e2da322faae6a1375f5e3637be40cdbf33446236
KregTM <kreis.roma@gmail.com>
2021-09-19 11:52:06 +0300

    Welcome to the club
    без гласных кароче

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NENVISHU GLASNIE: ");
+            string userInput = Console.ReadLine();
+
+            Glasnie(userInput);
+        }
+
+        static void Glasnie(string stringData)
+        {
+            string glasnie = "aAeEiIoOuUyY";
+            string result = "";
+            for (int i = 0; i < stringData.Length; i++)
+            {
+                if (!glasnie.Contains(stringData[i]))
+                {
+                    result = result + stringData[i];
+                }
+            }
+             Console.WriteLine("Welcome to the club, " + result);
+        }  
+    }
+}
